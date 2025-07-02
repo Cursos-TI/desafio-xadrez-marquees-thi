0f9d1af1b853da81b570243e192c28158f826868
marquees-thi <202505082054@alunos.estacio.br>
2025-07-02 17:35:48 +0000

    Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
-int main(){
-    int torre, bispo, rainha; 
-    int casasT, casasB, casasR; // Número de casas usadas para mover as peças (Torre, Bispo, Rainha)
-    int opcao; // Opção referente ao switch
-
-    printf("Seja bem vindo ao jogo de Xadrez\n");
-    printf("1. Torre\n");
-    printf("2. Bispo\n");
-    printf("3. Rainha\n");
-    printf("Qual peça deseja mover? (1-3): ");
-    scanf("%d", &opcao);
-
-    switch(opcao){
-    case 1:
-        printf("Deseja mover a Torre quantas casas à direita? (1-5): ");
+void torre(){
+    int torre;
+    int casasT;
+    printf("Deseja mover a Torre quantas casas à direita? (1-5): ");
         scanf("%d", &casasT);
         if(casasT <= 5){
             printf("\nMovimentando a Torre %d casas à direita.\n", casasT);
@@ -22,10 +11,14 @@ int main(){
                 printf("Direita\n");
             }
         }else{
-            printf("Digite um número de 1 a 5.");
-        }break;
-    case 2:
-        printf("Deseja mover o Bispo quantas casas à diagonal para direita? (1-5): ");
+            printf("Digite um número de 1 a 5.\n");
+        }
+}
+
+void bispo(){
+    int bispo;
+    int casasB;
+    printf("Deseja mover o Bispo quantas casas à diagonal para direita? (1-5): ");
         scanf("%d", &casasB);
 
         if(casasB <= 5){
@@ -34,10 +27,14 @@ int main(){
                 printf("Cima Direita\n");
             }
         }else{
-            printf("Digite um número de 1 a 5.");
-        }break;
-    case 3:
-        printf("Deseja mover a Rainha quantas casas à esquerda? (1-8): ");
+            printf("Digite um número de 1 a 5.\n");
+        }
+}
+
+void rainha(){
+    int rainha;
+    int casasR;
+    printf("Deseja mover a Rainha quantas casas à esquerda? (1-8): ");
         scanf("%d", &casasR);
 
         if(casasR <= 8){
@@ -46,8 +43,48 @@ int main(){
                 printf("Esquerda\n");
             }
         }else{
-            printf("Digite um número de 1 a 8.");
-        }break;
+            printf("Digite um número de 1 a 8.\n");
+        }
+}
+
+void cavalo(){
+    int cavalo; 
+    int casasC; 
+    printf("Iniciando o movimento do cavalo.\n");
+        casasC = 0;
+        while(casasC < 3){
+            for(casasC = 0; casasC < 2; casasC++){
+                printf("Cima\n");
+            }
+            printf("Direita\n");
+            casasC++;
+        }
+}
+
+int main(){
+    int opcao; // Opção referente ao switch
+
+    printf("Seja bem vindo ao jogo de Xadrez\n");
+    printf("1. Torre\n");
+    printf("2. Bispo\n");
+    printf("3. Rainha\n");
+    printf("4. Cavalo\n");
+    printf("Qual peça deseja mover? (1-4): ");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+    case 1:
+        torre();
+        break;
+    case 2:
+        bispo();
+        break;
+    case 3:
+        rainha();
+        break;
+    case 4:
+        cavalo();
+        break;
 
     default:
         printf("\nA opção escolhida não existe.");
